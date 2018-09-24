07ebcfb6daa805609e83940c12a6032ce6eab53f
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-09-24 21:24:23 +0200

    added copyright

--- app/src/main/java/de/dixieflatline/mpcw/services/implementation/SharedPreferencesService.java
+++ app/src/main/java/de/dixieflatline/mpcw/services/implementation/SharedPreferencesService.java
@@ -1,3 +1,19 @@
+/***************************************************************************
+ begin........: September 2018
+ copyright....: Sebastian Fedrau
+ email........: sebastian.fedrau@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v3 as published by
+ the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License v3 for more details.
+ ***************************************************************************/
 package de.dixieflatline.mpcw.services.implementation;
 
 import android.content.Context;
