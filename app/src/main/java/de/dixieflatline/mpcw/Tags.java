b85167d178d9aced7dad10149420ed14fa38d3f9
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-11-03 17:21:11 +0100

    changed header indentation

--- app/src/main/java/de/dixieflatline/mpcw/Tags.java
+++ app/src/main/java/de/dixieflatline/mpcw/Tags.java
@@ -1,18 +1,18 @@
 /***************************************************************************
- begin........: September 2018
- copyright....: Sebastian Fedrau
- email........: sebastian.fedrau@gmail.com
+    begin........: September 2018
+    copyright....: Sebastian Fedrau
+    email........: sebastian.fedrau@gmail.com
  ***************************************************************************/
 
 /***************************************************************************
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License v3 as published by
- the Free Software Foundation.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License v3 as published by
+    the Free Software Foundation.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License v3 for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License v3 for more details.
  ***************************************************************************/
 package de.dixieflatline.mpcw;
 
